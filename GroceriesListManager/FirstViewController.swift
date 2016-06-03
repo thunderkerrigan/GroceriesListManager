2cb0e787b6d696bfa5eabb6e2b18f743f2202161
Joseph <thunderkerrigan@gmail.com>
2016-06-03 12:57:58 +0200

    Initial Commit

---
+++ GroceriesListManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GroceriesListManager
+//
+//  Created by Joseph on 03/06/2016.
+//  Copyright © 2016 Thunderkerrigan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GroceriesListManager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GroceriesListManager
+//
+//  Created by Joseph on 03/06/2016.
+//  Copyright © 2016 Thunderkerrigan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
