aa6bf777c948023a87205a01ff50a031f337ee76
Joseph <joseph@MacBook-Pro-de-Joseph.local>
2016-06-21 14:12:24 +0200

    Swift 3

--- GroceriesListManager/FirstViewController.swift
+++ GroceriesListManager/FirstViewController.swift
@@ -8,18 +8,99 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
+class FirstViewController: UIViewController
+{
+    
+    @IBOutlet weak var tableView: UITableView?
+    var productsArray: NSMutableArray?
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView?.dataSource = self
+        tableView?.delegate = self
+        
+        if let path = Bundle.main().pathForResource("VegetablesByMonths", ofType: "json")
+        {
+            if let jsonData: Data = try! Data(contentsOf: URL(fileURLWithPath: path))
+            {
+                productsArray = try! JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions()) as? NSMutableArray
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func randomColor(_ number: NSInteger) -> UIColor {
+        switch number
+        {
+        case 0:
+            return UIColor.blue()
+        case 1:
+            return UIColor.red()
+        case 2:
+            return UIColor.purple()
+        case 3:
+            return UIColor.green()
+        case 4:
+            return UIColor.cyan()
+        default:
+            return UIColor.clear()
+        }
+    }
 }
 
+extension FirstViewController : UITableViewDataSource
+{
+    //MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        //TODO
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        //TODO
+        return productsArray!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let tableViewCell : UITableViewCell! = UITableViewCell()
+        tableViewCell.textLabel?.text = productsArray![(indexPath as NSIndexPath).row] as? String
+        tableViewCell.backgroundColor = self.randomColor(Int(arc4random_uniform(4)))
+        return tableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+}
+
+extension FirstViewController : UITableViewDelegate
+{
+    //MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]?
+    {
+        let tableViewRowAction: UITableViewRowAction? = UITableViewRowAction(style: .destructive , title: "Effacer", handler: { (_, indexPath) in
+            //TODO
+            self.productsArray!.removeObject(at: (indexPath as NSIndexPath).row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        })
+        return [tableViewRowAction!]
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let alertViewController: UIAlertController = UIAlertController(title: "Click", message: String(format: "click on %@", self.productsArray![(indexPath as NSIndexPath).row] as! String), preferredStyle: .alert)
+        alertViewController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+}
